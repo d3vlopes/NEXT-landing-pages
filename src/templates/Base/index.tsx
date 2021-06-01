6bae4a79972ced5b37f7dfb3f749105484bc2c34
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-05-31 23:36:15 -0300

    Change Templates for typescript

---
+++ src/templates/Base/index.tsx
@@ -0,0 +1,28 @@
+import { Menu } from '../../components/Menu';
+import { Footer } from '../../components/Footer';
+import { GoTop } from '../../components/GoTop';
+
+import { MenuLinkProps } from '../../components/MenuLink';
+import { LogoLinkProps } from '../../components/LogoLink';
+
+import * as Styled from './styles';
+
+export type BaseProps = {
+  children: React.ReactNode;
+  links: MenuLinkProps[];
+  footerHtml: string;
+  logoData: LogoLinkProps;
+};
+
+export const Base = ({ links, logoData, footerHtml, children }: BaseProps) => {
+  return (
+    <>
+      <Menu links={links} logoData={logoData} />
+      <Styled.Container>
+        {children}
+        <Footer footerHtml={footerHtml} />
+      </Styled.Container>
+      <GoTop />
+    </>
+  );
+};
---
+++ src/templates/Base/stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, Story } from '@storybook/react';
+import { Base, BaseProps } from '.';
+
+import { mockBase } from './mock';
+
+export default {
+  title: 'Templates/Base',
+  component: Base,
+  args: mockBase,
+} as Meta;
+
+export const Template: Story<BaseProps> = (args) => {
+  return (
+    <div>
+      <Base {...args} />
+    </div>
+  );
+};
